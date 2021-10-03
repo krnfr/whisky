d16d59d49c38d3cc5cec93db4e6f74ea392b08bf
kf <sabotagehase@gmail.com>
2021-10-03 18:47:07 +0200

    fixed price-info again

--- src/components/PriceInfo.vue
+++ src/components/PriceInfo.vue
@@ -7,21 +7,21 @@ const props = defineProps<{
   currency: number,
   convert: boolean
 }>()
-const c = ref(api.getCurrencyById(props.currency))
+const c = computed(() => api.getCurrencyById(props.currency))
 
 function valueFormatted() {
   return props.value / 100
 }
 function valueConverted() {
-  return ((props.value * c.value?.to_eur) / 100).toFixed(2)
+  return ((props.value * (c?.value?.to_eur ? c.value.to_eur : 1)) / 100).toFixed(2)
 }
 </script>
 
 <template>
   <div v-if="props.value">
     <span v-html="valueFormatted()" />
-    <span v-if="currency && c" v-html="' ' + c.symbol" />
-    <n-text depth="3" v-if="props.convert && currency > 1 && c">
+    <span v-if="currency" v-html="' ' + (c?.symbol ? c.symbol : '')" />
+    <n-text depth="3" v-if="props.convert && currency > 1">
       <span>&nbsp;/&nbsp;</span>
       <span v-html="valueConverted()" />
       <span>&nbsp;€</span>
