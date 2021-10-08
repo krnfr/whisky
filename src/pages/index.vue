5dd1073f35b66cc4b231ab12af78a19ebc4bf6aa
kf <sabotagehase@gmail.com>
2021-10-08 13:01:33 +0200

    added better index (;

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,5 +5,6 @@ const user = useUserStore()
 </script>
 
 <template>
-  <n-h1>hello (;</n-h1>
+  <n-h1>Hallo (;</n-h1>
+  <n-h2>die Open-Source Platform zum Verwalten und Verkaufen von Whisky's (+ alles andere zum trinken)</n-h2>
 </template>
