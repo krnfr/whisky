93b85ce3f8fddcde8438aa48020a41dd3b697e60
kf <sabotagehase@gmail.com>
2021-11-04 14:14:28 +0100

    added header and filter to CollectionList

--- src/components/CollectionList.vue
+++ src/components/CollectionList.vue
@@ -1,41 +1,92 @@
 <script setup lang="ts">
-import { useUserStore } from "~/stores/user";
+import { useSupabaseStore } from "~/stores/supabase"
+import { useUserStore } from "~/stores/user"
 import { CollectionItem } from "~/types"
 
 const router = useRouter()
 const user = useUserStore()
+const api = useSupabaseStore()
 
-defineProps<{
-  items: Array<CollectionItem>
+const selectedCategory = ref(null)
+const selectedLabel = ref(null)
+const onlySell = ref(false)
+
+const props = defineProps<{
+  items: Array<CollectionItem>,
+  noFilter: boolean,
 }>()
+
+
+const list = computed(() => {
+  let r = props.items
+  if (onlySell.value) r = r.filter(i => i.sell)
+  if (selectedCategory.value) r = r.filter(i => i.liquor?.category?.id == selectedCategory.value)
+  if (selectedLabel.value) r = r.filter(i => i.liquor?.category?.id == selectedLabel.value)
+  return r
+})
 </script>
 
-<template>
+<template class="m-5 p-3">
   <n-space vertical>
+    <n-page-header title="Sammlung" @back="() => null">
+      <n-grid v-if="!noFilter" cols="3" class="p-3">
+        <n-form-item-gi label="Kategory" class="w-50">
+          <n-select
+            v-model:value="selectedCategory"
+            :options="api.selectCategories()"
+            filterable
+            clearable
+          />
+        </n-form-item-gi>
+        <n-form-item-gi label="Label" class="w-50">
+          <n-select
+            v-model:value="selectedLabel"
+            :options="api.selectLabels()"
+            filterable
+            clearable
+          />
+        </n-form-item-gi>
+        <n-form-item-gi label="Nur Verkaufbar">
+          <n-switch v-model:value="onlySell" />
+        </n-form-item-gi>
+      </n-grid>
+    </n-page-header>
     <n-grid cols="1 m:3 xl:6" :x-gap="10" :y-gap="10" responsive="screen">
-      <template v-for="i in items">
+      <template v-for="i in list">
         <n-gi v-if="user.loggedIn || i.public">
           <n-card class="h-full" @click="router.push('/items/' + i.id)">
-            <n-thing
-              :key="i.id"
-              :title="(i.liquor.name ? i.liquor.name : i.liquor.label.name) + (i.version ? ' - ' + i.version : '') + (i.year ? (' (' + i.year + ')') : '')"
-            >
+            <n-thing :key="i.id">
+              <template #header>
+                <n-space>
+                  <n-text
+                    strong
+                    depth="2"
+                  >{{ (i.liquor.name ? i.liquor.name : i.liquor.label.name) + (i.version ? ' - ' + i.version : '') + (i.year ? (' (' + i.year + ')') : '') }}</n-text>
+                  <n-tag v-if="i.sell" type="primary" size="small">zu verkaufen</n-tag>
+                </n-space>
+              </template>
               <template #avatar>
-                <item-avatar :item="i.id" :image="i.cover" />
+                <item-avatar size="large" :item="i.id" :image="i.cover" />
               </template>
               <template #header-extra>
                 <n-space justify="end">
-                  <n-tag v-if="i.liquor.category?.name" size="small">
-                    {{
-                      i.liquor.category.name
-                    }}
-                  </n-tag>
+                  <n-text strong>
+                    <n-tag v-if="i.liquor.category?.name">
+                      {{
+                        i.liquor.category.name
+                      }}
+                    </n-tag>
+                  </n-text>
                 </n-space>
               </template>
               <template #description v-if="i.liquor.label">
-                {{
-                  i.liquor.label.name
-                }}
+                <n-space>
+                  <n-text>
+                    {{
+                      i.liquor.label.name
+                    }}
+                  </n-text>
+                </n-space>
               </template>
               <!-- {{ i.id }} -->
             </n-thing>
